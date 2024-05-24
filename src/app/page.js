433fd84c0ead7b786eca0b8799429b3326b3934b
sobhanbera <sobhanbera258@gmail.com>
2024-05-24 16:27:30 +0530

    website title updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 import FlowBuilder from '@/components/FlowBuilder';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Chatbot flow builder | BiteSpeed Frontend Task</title>
+      </Head>
+
       <FlowBuilder />
     </main>
   );
